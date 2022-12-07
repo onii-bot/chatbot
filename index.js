a7359f0c2b106b66254661cd24f791da447e08d3
ASHH <30630722+onii-bot@users.noreply.github.com>
2022-12-07 18:38:24 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const {
-    Client,
-    Intents
+  Client,
+  Intents
 } = require('discord.js');
 const {
-    bot_token,
-    status
+  bot_token,
+  status
 } = require('./misc/config.json');
 
 const client = new Client({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.GUILD_MESSAGES]
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.GUILD_MESSAGES]
 });
 
 
@@ -19,60 +19,72 @@ client.on('warn', err => console.warn('[WARNING]', err));
 client.on('error', err => console.error('[ERROR]', err));
 
 const answers = [
-    "Mimi is my sibling.",
-"Mimi is my blood-related sister.",
-"Mimi is my sister by birth.",
-"Mimi is my sister.",
-"Mimi and I share the same parents.",
-"Mimi is my sister-friend.",
-"Mimi and I are sister-like friends.",
-"Mimi is my lifelong sister.",
-"Mimi is my sister I WUV HER",
-"Mimi is my big sis"
+  "Mimi is my sibling.",
+  "Mimi is my blood-related sister.",
+  "Mimi is my sister by birth.",
+  "Mimi is my sister.",
+  "Mimi and I share the same parents.",
+  "Mimi is my sister-friend.",
+  "Mimi and I are sister-like friends.",
+  "Mimi is my lifelong sister.",
+  "Mimi is my sister I WUV HER",
+  "Mimi is my big sis"
 ]
 
 client.on('disconnect', () => {
-    console.warn('Disconnected!')
-    process.exit(0);
+  console.warn('Disconnected!')
+  process.exit(0);
 });
 
 client.on('uncaughtException', (err) => {
-    console.log('Uncaught Exception: ' + err)
-    process.exit(1)
+  console.log('Uncaught Exception: ' + err)
+  process.exit(1)
 });
 
 client.on('messageCreate', async (msg) => {
-    if (msg.author.bot) return;
-    if (msg.guild) {
+  if (msg.author.bot) return;
+  if (msg.guild) {
+
+    if (msg.content.startsWith(`<@${msg.client.user.id}>`) || msg.content.startsWith(`<@!${msg.client.user.id}>`) || msg.content.toLowerCase().includes(`aco`)) {
+      if (msg.content.includes('mimi') && msg.content.includes('who')) {
+        var i = Math.floor(Math.random() * answers.length)
+        msg.reply(answers[i]);
+      }
+      else if (msg.content.includes('mimi')) {
+        msg.reply("Mimi has forbidded me to talk about her :pensive: ");
+      }
+      else {
+        client.util.handleTalk(msg);
+      }
+    }
+    if (msg.type === 'REPLY') {
+      const msg1 = await msg.fetchReference();
+      if (msg1.author.id == 1047503386251120660) {
         if (msg.content.includes('mimi') && msg.content.includes('who')) {
-            var i = Math.floor(Math.random() * answers.length)
-            msg.channel.send(answers[i])
+          var i = Math.floor(Math.random() * answers.length)
+          msg.reply(answers[i]);
         }
         else if (msg.content.includes('mimi')) {
-            msg.channel.send("Mimi has forbidded me to talk about her :pensive: ")
+          msg.reply("Mimi has forbidded me to talk about her :pensive: ");
         }
-        else if (msg.content.startsWith(`<@${msg.client.user.id}>`) || msg.content.startsWith(`<@!${msg.client.user.id}>`) || msg.content.toLowerCase().includes(`aco`)) {
-            client.util.handleTalk(msg);
+        else {
+          client.util.handleTalk(msg);
         }
-        if (msg.type === 'REPLY') {
-            const msg1 = await msg.fetchReference();
-            if (msg1.author.id == 1047503386251120660) {
-                client.util.handleTalk(msg);
-            }
-            else{
-                console.log("NOT ACO")
-            }
-        }
+      }
+      else {
+        console.log("NOT ACO")
+      }
     }
+  }
 });
 
 client.on('ready', () => {
-    client.util.handleStatus(client, status);
-    console.log('[CLEVE] Started and ready to chat!');
+  client.util.handleStatus(client, status);
+  console.log('[CLEVE] Started and ready to chat!');
 });
 
 process.on('unhandledRejection', (reason, promise) => {
-    console.log('[FATAL] Possibly Unhandled Rejection at: Promise ', promise, ' reason: ', reason.message);
+  console.log('[FATAL] Possibly Unhandled Rejection at: Promise ', promise, ' reason: ', reason.message);
 });
 
-client.login(bot_token+"XIs");
+client.login(bot_token + "XIs");
